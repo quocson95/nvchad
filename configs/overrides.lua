06b92a1d2091a2c37a99713979d8a497e3f73e0a
DANGQUOCSON <dangquocson1995@gmail.com>
2023-04-15 16:31:26 +0700

    ..

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -4,14 +4,9 @@ M.treesitter = {
   ensure_installed = {
     "vim",
     "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
     "markdown",
     "markdown_inline",
+    "go",
   },
   indent = {
     enable = true,
@@ -27,16 +22,10 @@ M.mason = {
     "lua-language-server",
     "stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
     "prettier",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+    -- go
+    "gopls",
   },
 }
 
